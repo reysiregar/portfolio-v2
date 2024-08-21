2c64ba6281ceeefba73050bc40c0167968a4781f
levina-lab <levinashavila@gmail.com>
2024-08-21 07:48:58 +0700

    destination
    Signed-off-by: levina-lab <levinashavila@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
             </li>
             <li>
               <router-link to="/portfolio"
-                class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Portfolio</router-link>
+                class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Project</router-link>
             </li>
             <li>
               <router-link to="/blog"
