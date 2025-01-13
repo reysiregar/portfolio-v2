6882ab6e49ff6393991bd6168ffc8d09ddf1fed3
reysiregar <levinashavila@gmail.com>
2025-01-13 22:09:25 +0700

    [Dev] vercel adjustment
    Signed-off-by: reysiregar <levinashavila@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="max-w-7xl mx-auto flex flex-col relative">
+    <!-- Speed Insights Component -->
+    <SpeedInsights />
+    
     <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
       <div class="container mx-auto flex flex-wrap items-center justify-between">
         <button @click="redirectToHome" class="flex">
@@ -11,29 +14,22 @@
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
             <li>
-              <router-link to="/"
-                class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
-                aria-current="page">Home</router-link>
+              <router-link to="/" class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0" aria-current="page">Home</router-link>
             </li>
             <li>
-              <router-link to="/about"
-                class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">About</router-link>
+              <router-link to="/about" class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">About</router-link>
             </li>
             <li>
-              <router-link to="/portfolio"
-                class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Project</router-link>
+              <router-link to="/portfolio" class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Project</router-link>
             </li>
             <li>
-              <router-link to="/certificate"
-                class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Certificate</router-link>
+              <router-link to="/certificate" class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Certificate</router-link>
             </li>
             <li>
-              <router-link to="/blog"
-                class="fadein-bot fadein-4 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Blog</router-link>
+              <router-link to="/blog" class="fadein-bot fadein-4 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Blog</router-link>
             </li>
             <li>
-              <router-link to="/contact"
-                class="fadein-bot fadein-5 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Contact</router-link>
+              <router-link to="/contact" class="fadein-bot fadein-5 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Contact</router-link>
             </li>
           </ul>
         </div>
@@ -45,25 +41,30 @@
     </div>
   </div>
   <footer class="block md:hidden fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90">
-  <nav class="flex justify-around py-4 text-xs">
-    <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
-    <router-link to="/about" class="text-gray-300 hover:text-white">About</router-link>
-    <router-link to="/portfolio" class="text-gray-300 hover:text-white">Project</router-link>
-    <router-link to="/certificate" class="text-gray-300 hover:text-white">Certificate</router-link>
-    <router-link to="/blog" class="text-gray-300 hover:text-white">Blog</router-link>
-    <router-link to="/contact" class="text-gray-300 hover:text-white">Contact</router-link>
-  </nav>
-</footer>
+    <nav class="flex justify-around py-4 text-xs">
+      <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
+      <router-link to="/about" class="text-gray-300 hover:text-white">About</router-link>
+      <router-link to="/portfolio" class="text-gray-300 hover:text-white">Project</router-link>
+      <router-link to="/certificate" class="text-gray-300 hover:text-white">Certificate</router-link>
+      <router-link to="/blog" class="text-gray-300 hover:text-white">Blog</router-link>
+      <router-link to="/contact" class="text-gray-300 hover:text-white">Contact</router-link>
+    </nav>
+  </footer>
 </template>
 
 <script>
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+
 export default {
+  components: {
+    SpeedInsights,
+  },
   methods: {
     redirectToHome() {
-      this.$router.push('/')
-    }
+      this.$router.push('/');
+    },
   },
-}
+};
 </script>
 
 <style>
@@ -90,7 +91,9 @@ export default {
   border-radius: 5px;
 }
 
-::-webkit-scrollbar-button { width: 20px; }
+::-webkit-scrollbar-button {
+  width: 20px;
+}
 
 body {
   font-family: 'Poppins', sans-serif;
@@ -133,7 +136,6 @@ nav a.router-link-exact-active:hover {
   color: white;
 }
 
-
 @keyframes fadeInLeft {
   0% {
     opacity: 0;
@@ -144,5 +146,4 @@ nav a.router-link-exact-active:hover {
     transform: translateX(0);
   }
 }
-
 </style>
